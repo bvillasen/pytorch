26cfe0e263afc5731609d6ea845b1aade62b9c5f
Deng, Daisy <daisy.deng@intel.com>
2025-11-25 15:31:02 +0000

    port _check_arg_device() to xpu

--- torch/nn/modules/activation.py
+++ torch/nn/modules/activation.py
@@ -1058,6 +1058,7 @@ def _check_arg_device(x: Optional[torch.Tensor]) -> bool:
         return x.device.type in [
             "cpu",
             "cuda",
+            "xpu",
             torch.utils.backend_registration._privateuse1_backend_name,
         ]
     return True
